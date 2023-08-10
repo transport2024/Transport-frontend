70e12163caacfd5b22738a4af466658419355a07
Jayasuryan99 <jayasuryan99@gmail.com>
2023-08-10 20:52:04 +0530

    final

--- src/components2/memoentry.jsx
+++ src/components2/memoentry.jsx
@@ -71,7 +71,7 @@ function Memo() {
     if (updateId === "") {
       try {
         const formData = {
-          gcno: Memo.length+1,
+          gcno: Memo.length+121,
           drivername: value.drivername,
           date: value.date,
           vehicleno: value.vehicleno,
