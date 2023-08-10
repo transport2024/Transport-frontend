65999f668df2bd16fda18ea8746b6d7aedd3b95d
ragul0427 <ragulhp2704@gmail.com>
2023-08-10 05:56:54 -0700

    rgnrgr

--- src/components2/memoentry.jsx
+++ src/components2/memoentry.jsx
@@ -71,7 +71,7 @@ function Memo() {
     if (updateId === "") {
       try {
         const formData = {
-          gcno: result.length+1,
+          gcno: memo.length+1,
           drivername: value.drivername,
           date: value.date,
           vehicleno: value.vehicleno,
