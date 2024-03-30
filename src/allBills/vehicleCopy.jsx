e11e59a71fa7efb075ec02ac420765f7c3140f36
ragul0427 <ragulhp2704@gmail.com>
2024-03-30 22:48:32 +0530

    bill final

--- src/allBills/vehicleCopy.jsx
+++ src/allBills/vehicleCopy.jsx
@@ -66,7 +66,7 @@ setFilterConsignee(
  
 
   return (
-    <div className="bg-white w-screen pt-[2px] h-[89vh] relative" style={{zIndex:999}}>
+    <div className="bg-white w-screen !pt-[40px] h-[89vh] relative" style={{zIndex:999}}>
     <div className="w-[98vw] border m-auto border-black h:[75vh]">
       <div className="flex  pl-10 pt-5 text-[14px]">
         <div>
