b950e6cab7736bcdbb8c21e3e0a1fc92c8051e33
ragul0427 <ragulhp2704@gmail.com>
2024-03-30 17:12:33 +0530

    fix

--- src/allBills/driver.jsx
+++ src/allBills/driver.jsx
@@ -68,7 +68,7 @@ function Driver(props) {
       className="bg-white w-screen pt-2 !h-[95vh] relative"
       style={{ zIndex: 999 }}
     >
-      <div className="w-[98vw] border m-auto border-black h:[80vh] ">
+      <div className="w-[98vw] border m-auto border-black h:[75vh] ">
         <div className="flex  pl-10 pt-5 text-[14px]">
           <div>
             <Image src={image} preview={false} width={90} alt="logo" />
