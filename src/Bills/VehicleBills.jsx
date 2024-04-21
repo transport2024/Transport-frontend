3bc05038016464266d9b5ccc083ad243b22852a3
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2024-04-21 12:26:34 +0530

    Update VehicleBills.jsx

--- src/Bills/VehicleBills.jsx
+++ src/Bills/VehicleBills.jsx
@@ -87,10 +87,12 @@ function VehicleBills() {
             JAI BALAJI ROADWAYS
           </h1>
           <p>
-            <strong> Super Jet Line Service for Tamilnadu, Karnataka, Maharashtra, Gujarat</strong>
+            <strong> Commission Agent </strong>
           </p>
-          <p> Shop No.1, H.No.:5-29/2, Near Railway Bridge, NH-44, THONDUPALLY(V), SHAMSHABAD(M), Ranga Reddy District- 509 325. HYDRABAD (Telangana.)
-           Cell: 91525 25000, 91525 26000 E-Mail:hydjaibalaji@gmail.com</p>
+          <p> H.O : Shop No.1, H.No.:5-29/2, Near Railway Bridge, NH-44, THONDUPALLY(V), SHAMSHABAD(M), Ranga Reddy District- 509 325. HYDRABAD (Telangana.)
+              B.O :  No.4/181,Plot No. 8,1st Floor Sri Ram Tower,Thuraiyur Road,Anna Nagar,NAMAKKAL-637 002.
+</p>           
+            <p> Cell: 91525 25000, 91525 26000 E-Mail:hydjaibalaji@gmail.com </p>
         </div>
       </div>
       <div>
