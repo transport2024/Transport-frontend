5436d8699e0019b7a2d5920a64d11d4aaa114193
Jayasuryan <127474567+Jayasuryan99@users.noreply.github.com>
2024-04-21 11:46:04 +0530

    Update Conisgnor.jsx

--- src/allBills/Conisgnor.jsx
+++ src/allBills/Conisgnor.jsx
@@ -84,13 +84,12 @@ function Conisgnor(props) {
             JAI BALAJI ROADWAYS
           </h1>
           <p>
-            <strong>Transport Contractors</strong>
+            <strong> Super Jet Line Service for Tamilnadu, Karnataka, Maharashtra, Gujarat </strong>
           </p>
           <p>
-            No.4/181,Plot No. 8,1st Floor Sri Ram Tower,Thuraiyur Road,Anna
-            Nagar,NAMAKKAL-637 002
+           Shop No.1, H.No.:5-29/2, Near Railway Bridge, NH-44, THONDUPALLY(V), SHAMSHABAD(M), Ranga Reddy District- 509 325. HYDRABAD (Telangana.)
           </p>
-          <p>Cell:91525 26000, 91525 28000 E-Mail:rajshreenkl@gmail.com</p>
+          <p>Cell:91525 25000, 91525 26000 E-Mail:hydjaibalaji@gmail.com</p>
         </div>
       </div>
       <div>
@@ -101,7 +100,7 @@ function Conisgnor(props) {
             </span>
             <input
               type="text"
-              style={{ width: "25vw", borderColor: "black" }}
+              style={{ width: "20vw", borderColor: "black" }}
               defaultValue={inputs?.lorryno}
               className="outline-none pl-10 !bg-transparent border-b-2 uppercase"
             />
@@ -154,7 +153,7 @@ function Conisgnor(props) {
            <span className="">Consignor:</span>
             <input
               type="text"
-              style={{ width: "38vw", borderColor: "black" }}
+              style={{ width: "54vw", borderColor: "black" }}
               defaultValue={inputs.consignor}
               className="outline-none pl-10 !bg-transparent border-b-2"
             />
@@ -175,7 +174,7 @@ function Conisgnor(props) {
             <span className="">Consignee:</span>
             <input
               type="text"
-              style={{ width: "38vw", borderColor: "black" }}
+              style={{ width: "54vw", borderColor: "black" }}
               defaultValue={inputs.consignee}
               className="outline-none pl-10 !bg-transparent border-b-2"
             />
